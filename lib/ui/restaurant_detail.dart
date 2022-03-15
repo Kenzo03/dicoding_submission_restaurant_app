51cb0aefe038fa89bad6b2f56702f01752864b15
kenzo03 <kevinwijaya480@gmail.com>
2022-03-15 13:26:39 +0700

    add sliver app bar to detail screen

--- lib/ui/restaurant_detail.dart
+++ lib/ui/restaurant_detail.dart
@@ -12,16 +12,42 @@ class RestaurantDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text(restaurant.name)),
-        body: SingleChildScrollView(
-            child: Column(
-          children: [
-            Text(restaurant.description),
-            const SizedBox(
-              height: 8,
-            ),
-            Text(restaurant.city)
-          ],
-        )));
+        // appBar: AppBar(title: Text(restaurant.name)),
+        body: NestedScrollView(
+            headerSliverBuilder: (context, isScrolled) {
+              return [
+                SliverAppBar(
+                  pinned: true,
+                  expandedHeight: 200,
+                  flexibleSpace: FlexibleSpaceBar(
+                      background: Image.network(restaurant.pictureId,
+                          fit: BoxFit.fitWidth),
+                      title: Text(restaurant.name),
+                      titlePadding:
+                          const EdgeInsets.only(left: 48, bottom: 16)),
+                )
+              ];
+            },
+            body: ListView(
+              children: [
+                Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [Text(restaurant.description)],
+                    ))
+              ],
+            ))
+        // SingleChildScrollView(
+        //     child: Column(
+        //   children: [
+        //     Text(restaurant.description),
+        //     const SizedBox(
+        //       height: 8,
+        //     ),
+        //     Text(restaurant.city)
+        //   ],
+        // ))
+        );
   }
 }
